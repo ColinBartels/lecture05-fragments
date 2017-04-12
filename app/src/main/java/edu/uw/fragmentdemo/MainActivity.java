e7f95c196b96725fff754b0f95c1fd4b8cc476c1
Colin Bartels <colin.0011@hotmail.com>
2017-04-11 19:19:17 -0700

    finished lab

--- app/src/main/java/edu/uw/fragmentdemo/MainActivity.java
+++ app/src/main/java/edu/uw/fragmentdemo/MainActivity.java
@@ -1,10 +1,13 @@
 package edu.uw.fragmentdemo;
 
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
@@ -12,44 +15,45 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity implements MoviesFragment.OnMovieSelectedListener, SearchFragment.OnSearchSubmittedListener {
 
     private static final String TAG = "MainActivity";
+    private int pages = 1;
+
+    private ViewPager pager;
+    private PagerAdapter pagerAdapter;
+
+    SearchFragment searchFragment;
+    DetailFragment detailFragment;
+    MoviesFragment moviesFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        // Instantiate a ViewPager and a PagerAdapter.
+        pager = (ViewPager) findViewById(R.id.container);
+        pagerAdapter = new MoviePagerAdapter(getSupportFragmentManager());
+        pager.setAdapter(pagerAdapter);
 
-    //respond to search button clicking
-    public void handleSearchClick(View v){
-        EditText text = (EditText)findViewById(R.id.txtSearch);
-        String searchTerm = text.getText().toString();
-
-        //add a new results fragment to the page
-        MoviesFragment fragment = MoviesFragment.newInstance(searchTerm);
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.container, fragment, "MoviesFragment");
-        ft.addToBackStack(null); //remember for the back button
-        ft.commit();
+        searchFragment = SearchFragment.newInstance();
+        pagerAdapter.notifyDataSetChanged();
+        pager.setCurrentItem(0);
+        pages = 1;
     }
 
     @Override
     public void onMovieSelected(Movie movie) {
-        DetailFragment detailFragment = DetailFragment.newInstance(movie.toString(), movie.imdbId);
-
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.container, detailFragment, null)
-                .addToBackStack(null)
-                .commit();
+        detailFragment = DetailFragment.newInstance(movie.toString(), movie.imdbId);
+        pager.setCurrentItem(2);
+        pages = 3;
+        pagerAdapter.notifyDataSetChanged();
     }
 
     @Override
     public void onSearchSubmitted(String searchTerm) {
-        MoviesFragment movieFragment = MoviesFragment.newInstance(searchTerm);
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.listView, movieFragment, null)
-                .addToBackStack(null)
-                .commit();
+        moviesFragment = MoviesFragment.newInstance(searchTerm);
+        pager.setCurrentItem(1);
+        pages = 2;
+        pagerAdapter.notifyDataSetChanged();
     }
 
     private class MoviePagerAdapter extends FragmentStatePagerAdapter {
@@ -60,12 +64,26 @@ public class MainActivity extends AppCompatActivity implements MoviesFragment.On
 
         @Override
         public Fragment getItem(int position) {
-            return null;
+            switch (position) {
+                case 0:
+                    return searchFragment;
+                case 1:
+                    return moviesFragment;
+                case 2:
+                    return detailFragment;
+                default:
+                    return searchFragment;
+            }
         }
 
         @Override
         public int getCount() {
-            return 0;
+            return pages;
+        }
+
+        @Override
+        public int getItemPosition(Object object) {
+            return POSITION_NONE;
         }
     }
 }
